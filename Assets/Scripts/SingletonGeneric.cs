7bce303fba3ac56ca001ffb3fe2574eb493d8f51
mridul-agrawal <48617300+mridul-agrawal@users.noreply.github.com>
2021-12-02 11:20:41 +0530

    Singleton Generic Class and derived singleton class Implementation.

---
+++ Assets/Scripts/SingletonGeneric.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonGeneric<T> : MonoBehaviour where T: SingletonGeneric<T>
+{
+    public static T instance;
+    public static T Instance { get { return instance; } }
+
+    protected virtual void Awake()
+    {
+        if(instance == null)
+        {
+            instance = (T)this;
+        } else
+        {
+            Destroy(this);
+        }
+    }
+}
+
+
+public class DerivedInheritence : SingletonGeneric<DerivedInheritence>
+{
+    protected override void Awake()
+    {
+        base.Awake();
+        //Custom Logic
+    }
+}
